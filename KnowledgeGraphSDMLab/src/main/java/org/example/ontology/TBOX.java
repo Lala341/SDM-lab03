007f0a3468bf5a724454c048b89973e11c15823b
lala341 <li.forero@uniandes.edu.co>
2023-05-06 03:56:04 -0500

    changes a models

--- KnowledgeGraphSDMLab/src/main/java/org/example/ontology/TBOX.java
+++ KnowledgeGraphSDMLab/src/main/java/org/example/ontology/TBOX.java
@@ -8,46 +8,40 @@ import org.apache.jena.rdf.model.ModelFactory;
 
 
 public class TBOX {
-    public static final String BASE_URI = "http://www.bdma.upc/#";
-    public static final String TBOX_MODEL_PATH = "data/publications.owl";
-    public static final String ABOX_MODEL_PATH = "data/publications_data.nt";
-    public static final String DATA_FILE_PATH = "data/raw/instances_data.csv";
+    public static final String BASE = "http://www.sdm.upc.edu/#";
 
-    // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModel.html
-    // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/ontology/OntModelSpec.html
-    // https://jena.apache.org/documentation/javadoc/jena/org/apache/jena/rdf/model/ModelFactory.html#createOntologyModel(org.apache.jena.ontology.OntModelSpec)
+    public static void createTBOX(){
 
-    OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF);
-    //==================================
-    // Ontology for Persons
-    //==================================
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF);
+       
+        OntClass person = model.createClass(BASE.concat("Person") );
+        OntClass author = model.createClass(BASE.concat("Author") );
+        OntClass chair = model.createClass( BASE.concat("Chair") );
+        OntClass editor = model.createClass(BASE.concat("Editor") );
+        OntClass reviewer = model.createClass( BASE.concat("Reviewer") );
 
-    OntClass persona = model.createClass(BASE_URI.concat("Person") );
-    OntClass author = model.createClass(BASE_URI.concat("Author") );
-    OntClass chair = model.createClass( BASE_URI.concat("Chair") );
-    OntClass editor = model.createClass(BASE_URI.concat("Editor") );
-    OntClass reviewer = model.createClass( BASE_URI.concat("Reviewer") );
-
-    persona.addSubClass( author );
-    persona.addSubClass( chair );
-        persona.addSubClass( editor );
-        persona.addSubClass( reviewer );
-
-    //==================================
-    // Ontology for Papers
-    //==================================
-
-    OntClass paper = model.createClass( constants.BASE_URI.concat("Paper") );
-    OntClass fullPaper = model.createClass( constants.BASE_URI.concat("Full_Paper") );
-    OntClass shortPaper = model.createClass( constants.BASE_URI.concat("Short_Paper") );
-    OntClass demoPaper = model.createClass( constants.BASE_URI.concat("Demo_Paper") );
-    OntClass posterPaper = model.createClass( constants.BASE_URI.concat("Poster_Paper") );
+        person.addSubClass( author );
+        person.addSubClass( chair );
+        person.addSubClass( editor );
+        person.addSubClass( reviewer );
+        
+        OntClass paper = model.createClass( BASE.concat("Paper") );
+        OntClass fullPaper = model.createClass( BASE.concat("Full_Paper") );
+        OntClass shortPaper = model.createClass( BASE.concat("Short_Paper") );
+        OntClass demoPaper = model.createClass( BASE.concat("Demo_Paper") );
+        OntClass posterPaper = model.createClass( BASE.concat("Poster_Paper") );
 
         paper.addSubClass( fullPaper );
         paper.addSubClass( shortPaper );
         paper.addSubClass( demoPaper );
-        paper.addSubClass( posterPaper ); // Only for conference papers
+        paper.addSubClass( posterPaper );
 
+
+        
+        
+
+
+    }
 
 
 }
