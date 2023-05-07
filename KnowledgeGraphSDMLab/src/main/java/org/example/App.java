06a34fc5df2a7bde78addbaebecefca05da66e17
zivkovicbogdana <bogdanaz207@gmail.com>
2023-05-07 10:56:09 +0200

    Abox

--- KnowledgeGraphSDMLab/src/main/java/org/example/App.java
+++ KnowledgeGraphSDMLab/src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import org.example.ontology.ABOX;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        ABOX.createABOX();
     }
 }
